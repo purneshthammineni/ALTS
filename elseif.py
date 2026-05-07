b9ea7904c99bffc5ac1eef4ddb8d634326ecf488
purnesh <purneshtammineni@gmail.com>
2026-05-07 10:13:59 +0530

    Delete elseif.py

--- elseif.py
+++
@@ -1,9 +0,0 @@
-x = int(input("enter the value of x:"))
-if x > 10:
-    print("x is greater than 10")
-elif x == 10:
-        print("x is equal to 10")
-
-else:
-    print("x is lessthan 10")
-    
